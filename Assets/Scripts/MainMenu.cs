fde3f55136cc76260dd73b651cefe9e958feae25
macmitt2300@gmail.com <macmitt2300@gmail.com>
2017-10-28 16:15:32 -0400

    Continue to work on main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 { 
@@ -13,9 +14,23 @@ public class MainMenu : MonoBehaviour
         Sprite[] thumbnails = Resources.LoadAll<Sprite>("Levels");
         foreach(Sprite thumbnail in thumbnails)
         {
-            GameObject container = Instantiate(levelButtonPrefab) as GameObject;
-            container.GetComponent<Image>().sprite = thumbnail;
-            container.transform.SetParent(levelButtonContainer.transform, false);
+            GameObject levelButton = Instantiate(levelButtonPrefab) as GameObject;
+            levelButton.GetComponent<Image>().sprite = thumbnail;
+            levelButton.transform.SetParent(levelButtonContainer.transform, false);
+
+            string sceneName = thumbnail.name; // Scene name
+            levelButton.GetComponent<Button>().onClick.AddListener(() => LoadLevel(sceneName));
         }
+    }
+
+    private void LoadLevel(string sceneName)
+    {
+        Debug.Log(sceneName); // Debugging
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LookAtMenu(string menu)
+    {
+
     }
 }
