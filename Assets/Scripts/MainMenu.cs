69c707f7d70a7693a84d153ddfad36de92cb3b5f
macmitt2300@gmail.com <macmitt2300@gmail.com>
2017-10-29 01:53:29 -0400

    Displays shop images and creating player

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,14 +6,22 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 { 
+    private const float CAMERA_TRANSITION_SPEED = 3.0f;
+
     public GameObject levelButtonPrefab;
     public GameObject levelButtonContainer;
+
+    public GameObject shopButtonPrefab;
+    public GameObject shopButtonContainer;
+    public Material playerMaterial;
 
     private Transform cameraTransform;
     private Transform cameraDesiredLookAt;
 
     private void Start()
     {
+        ChangePlayerSkin(10);
+
         cameraTransform = Camera.main.transform;
         Sprite[] thumbnails = Resources.LoadAll<Sprite>("Levels");
         foreach(Sprite thumbnail in thumbnails)
@@ -25,6 +33,14 @@ public class MainMenu : MonoBehaviour
             string sceneName = thumbnail.name; // Scene name
             levelButton.GetComponent<Button>().onClick.AddListener(() => LoadLevel(sceneName));
         }
+
+        Sprite[] textures = Resources.LoadAll<Sprite>("Player");
+        foreach(Sprite texture in textures)
+        {
+            GameObject textureButton = Instantiate(shopButtonPrefab) as GameObject;
+            textureButton.GetComponent<Image>().sprite = texture;
+            textureButton.transform.SetParent(shopButtonContainer.transform, false);
+        }
     }
 
     private void Update()
@@ -32,7 +48,7 @@ public class MainMenu : MonoBehaviour
         if(cameraDesiredLookAt != null)
         {
             // Adding rotation effect to camera in 3d space
-            cameraTransform.rotation = Quaternion.Slerp(cameraTransform.rotation, cameraDesiredLookAt.rotation, 3 * Time.deltaTime);
+            cameraTransform.rotation = Quaternion.Slerp(cameraTransform.rotation, cameraDesiredLookAt.rotation, CAMERA_TRANSITION_SPEED * Time.deltaTime);
         }
     }
 
@@ -40,6 +56,14 @@ public class MainMenu : MonoBehaviour
     {
         Debug.Log(sceneName); // Debugging
         SceneManager.LoadScene(sceneName);
+    }
+
+    private void ChangePlayerSkin(int index)
+    {
+        float x = (index % 4) * 0.25f; // modulus of 4 (how many rows) times 0.25f (division of sprites from image (ever 25% of the row) ) 
+        float y = (index / 4) * 0.25f; // now for columns
+
+        playerMaterial.SetTextureOffset("_MainTex", new Vector2(x, y));
     }
 
     public void LookAtMenu(Transform menuTransform)
