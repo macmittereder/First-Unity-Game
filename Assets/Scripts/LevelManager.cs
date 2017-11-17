3b8b998e8e84aaaaceb8d08763f2ebff2a16ceb2
macmitt2300@gmail.com <macmitt2300@gmail.com>
2017-11-17 07:59:30 -0500

    Working on player movement

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    private void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    public void TogglePauseMenu()
+    {
+        // If the button is on it gets turned off
+        pauseMenu.SetActive(!pauseMenu.activeSelf);
+    }
+
+    public void ToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ Assets/Scripts/Motor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Motor : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+    public float drag = 0.5f;
+    public float terminalRotationSpeed = 25.0f;
+
+    private Rigidbody controller;
+
+    private void Start()
+    {
+        controller = GetComponent<Rigidbody>();
+        controller.maxAngularVelocity = terminalRotationSpeed;
+        controller.drag = drag;
+    }
+
+    private void Update()
+    {
+        Vector3 dir = Vector3.zero;
+
+        dir.x = Input.GetAxis("Horizontal");
+        dir.y = Input.getAxis("Vertical");
+
+        if (dir.magnitude > 1)
+            dir.Normalize();
+
+        controller.AddForce(dir * moveSpeed);
+    }
+}
