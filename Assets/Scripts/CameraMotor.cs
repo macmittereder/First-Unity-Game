c9243d0dd623bb1a33de602ab74759641b1657a2
macmitt2300@gmail.com <macmitt2300@gmail.com>
2017-11-20 08:56:43 -0500

    Working on virtual joystick

--- Assets/Scripts/CameraMotor.cs
+++ Assets/Scripts/CameraMotor.cs
@@ -6,7 +6,9 @@ public class CameraMotor : MonoBehaviour
 {
     public Transform lookAt;
 
+    private Vector3 desiredPosition;
     private Vector3 offset;
+    private float smoothSpeed = 7.5f;
     private float distance = 5.0f;
     private float yOffset = 3.5f;
 
@@ -17,6 +19,25 @@ public class CameraMotor : MonoBehaviour
 
     private void Update()
     {
-        transform.position = lookAt.position + offset;
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            SlideCamera(true);
+        else if (Input.GetKey(KeyCode.RightArrow))
+            SlideCamera(false);
+
+    }
+
+    private void FixedUpdate()
+    {
+        desiredPosition = lookAt.position + offset;
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.LookAt(lookAt.position + Vector3.up);
+    }
+
+    public void SlideCamera(bool left)
+    {
+        if (left)
+            offset = Quaternion.Euler(0, 90, 0) * offset;
+        else
+            offset = Quaternion.Euler(0, -90, 0) * offset;
     }
 }
---
+++ Assets/Scripts/VirtualJoystick.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class VirtualJoystick : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+    private Image bgImage;
+    private Image joystickImage;
+
+    public Vector3 InputDirection { set; get; }
+
+    // Creating joystick logic
+
+    private void Start()
+    {
+        bgImage = GetComponent<Image>();
+        joystickImage = GetComponentInChildren<Image>();
+        InputDirection = Vector3.zero;
+    }
+
+    public virtual void OnDrag(PointerEventData ped)
+    {
+        // Moving the joystick
+        Vector2 pos = Vector2.zero;
+        if(RectTransformUtility.ScreenPointToLocalPointInRectangle(bgImage.rectTransform,
+            ped.position, ped.pressEventCamera, out pos))
+        {
+            pos.x = (pos.x / bgImage.rectTransform.sizeDelta.x);
+            pos.y = (pos.y / bgImage.rectTransform.sizeDelta.y);
+
+            float x = (bgImage.rectTransform.pivot.x == 1) ? pos.x * 2 + 1 : pos.x * 2 - 1;
+            float y = (bgImage.rectTransform.pivot.y == 1) ? pos.y * 2 + 1 : pos.y * 2 - 1;
+
+            InputDirection = new Vector3(x, 0, y);
+            InputDirection = (InputDirection.magnitude > 1) ? InputDirection.normalized : InputDirection;
+
+            joystickImage.rectTransform.anchoredPosition = new Vector3(InputDirection.x * (bgImage.rectTransform.sizeDelta.x / 3),
+                InputDirection.z * (bgImage.rectTransform.sizeDelta.y / 3));
+        }
+
+    }
+
+    public virtual void OnPointerDown(PointerEventData ped)
+    {
+
+    }
+
+    public virtual void OnPointerUp(PointerEventData ped)
+    {
+
+    }
+
+}
